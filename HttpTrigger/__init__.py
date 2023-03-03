a123d4777a517a0ce31f6d37371ea9505f0883f9
Govardhan0235 <81092877+Govardhan0235@users.noreply.github.com>
2023-03-03 11:43:45 +0530

    Update __init__.py

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -28,10 +28,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # create blob service client and container client
     credential = DefaultAzureCredential()
-    storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
+    storage_account_url = "https://aksdemorg87eb.blob.core.windows.net"
     client = BlobServiceClient(account_url=storage_account_url, credential=credential)
     blob_name = "test" + str(datetime.now()) + ".txt"
-    blob_client = client.get_blob_client(container=os.environ["par_storage_container_name"], blob=blob_name)
+    blob_client = client.get_blob_client(container=samplecontainer, blob=blob_name)
     blob_client.upload_blob(link_list)
 
     return func.HttpResponse(
